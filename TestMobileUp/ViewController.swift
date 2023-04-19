263e667d192ecdc9c98a1e8b41be037a55d8bc17
Andrew <agent56g@gmail.com>
2023-04-19 17:35:06 +0300

    Initial Commit

---
+++ TestMobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMobileUp
+//
+//  Created by Andrew Kvasha on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
