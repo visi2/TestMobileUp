792573fe23e20957979c8304f09da71c2c6e1583
Andrew <agent56g@gmail.com>
2023-04-19 17:59:31 +0300

    remove storyboard

--- TestMobileUp/ViewController.swift
+++ TestMobileUp/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  TestMobileUp
-//
-//  Created by Andrew Kvasha on 19.04.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
